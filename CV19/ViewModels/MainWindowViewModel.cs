79a09176d79221787e7e93e457cfc0b993106fa3
Dmytro Shaban <10shaban72@gmail.com>
2021-05-28 20:17:18 +0300

    Test ViewModel work

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Window Title
+        private string _Title = "Анализ статистики CV19";
+        
+        /// <summary>
+        /// Window title
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //if (Equals(_Title, value))
+            //{
+            //    return;
+            //}
+            //_Title = value;
+            //OnPropertyChanged();
+
+            //Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
