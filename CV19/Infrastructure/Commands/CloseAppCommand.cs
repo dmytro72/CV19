50acdb0c002933707a358e00afdc5aeb0cf2b2e8
Dmytro Shaban <10shaban72@gmail.com>
2021-05-29 15:50:06 +0300

    Create class for command CloseApp

---
+++ CV19/Infrastructure/Commands/CloseAppCommand.cs
@@ -0,0 +1,12 @@
+﻿using CV19.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace CV19.Infrastructure.Commands
+{
+    internal class CloseAppCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
