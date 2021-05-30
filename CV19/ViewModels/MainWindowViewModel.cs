ce49fa23e7b723f6dc89bdf02f855633352e6da7
Dmytro Shaban <10shaban72@gmail.com>
2021-05-30 17:26:17 +0300

    Add test data to ViewModel

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,6 +10,24 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region TestDataPoints
+        
+        /// <summary>
+        /// Test data for plot
+        /// </summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        /// <summary>
+        /// Test data for plot
+        /// </summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+
+        #endregion
+
         #region Window Title
 
         private string _Title = "Анализ статистики CV19";
@@ -80,6 +101,16 @@ namespace CV19.ViewModels
 
             #endregion
 
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                double y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x , Yvalue = y }); 
+            }
+
+            TestDataPoints = data_points;
         }
     }
 }
