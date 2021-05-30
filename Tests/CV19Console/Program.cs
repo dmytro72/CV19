dcdd9c071f58ab0db33d3fdd4dd8ae3b00290a6a
Dmytro Shaban <10shaban72@gmail.com>
2021-05-30 13:54:13 +0300

    Request to server and get raw data

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CV19Console
 {
     class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //WebClient client = new WebClient();
+
+            var client = new HttpClient();
+
+            HttpResponseMessage response = client.GetAsync(data_url).Result;
+            string csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
